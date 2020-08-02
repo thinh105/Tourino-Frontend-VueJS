25cc327ad968b1d5eef4c6a7640d77dfb7460416
thinhnv <maxiqboy@gmail.com>
2020-08-02 23:22:44 +0700

    :contruction: [WIP] filter tours
    quick prototype for filter function!

--- src/components/Tours/Filter.vue
+++ src/components/Tours/Filter.vue
@@ -1,18 +1,59 @@
 <template>
-  <v-container id="dropdown-example-1">
-    <v-overflow-btn
-      class="my-2"
-      :items="dropdown_font"
-      label="Overflow Btn"
-      target="#dropdown-example-1"
-    />
-  </v-container>
+  <v-row align="center" justify="center" no-gutters>
+    <v-col class="zing">
+      <v-subheader>Price</v-subheader>
+
+      <v-range-slider
+        v-model="range"
+        :max="max"
+        :min="min"
+        step="100"
+        ticks
+        thumb-label="always"
+        class="align-center zing"
+      />
+    </v-col>
+
+    <v-col>
+      <v-col class="d-flex zing">
+        <v-select clearable :items="rating" label="Rating" />
+      </v-col>
+    </v-col>
+
+    <v-col>
+      <v-col class="d-flex zing">
+        <v-select clearable :items="duration" label="Duration" />
+      </v-col>
+    </v-col>
+
+    <v-col>
+      <v-col class="d-flex zing">
+        <v-select clearable :items="duration" label="Duration" />
+      </v-col>
+    </v-col>
+
+    <v-col>
+      <v-col class="d-flex">
+        <v-select clearable multiple :items="duration" label="Duration" />
+      </v-col>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
   export default {
     data: () => ({
-      dropdownfont: ['Arial', 'Calibri', 'Courier', 'Verdana'],
+      min: 0,
+      max: 1000,
+      range: [0, 1000],
+      rating: ['⭐⭐⭐⭐ & Up', '⭐⭐⭐ & Up', '⭐⭐ & Up', '⭐ & Up'],
+      duration: ['1 day', '1 to 3 days', '4 to 7 days', '7+ days'],
     }),
   };
 </script>
+
+<style scoped>
+  .zing {
+    width: 220px;
+  }
+</style>
