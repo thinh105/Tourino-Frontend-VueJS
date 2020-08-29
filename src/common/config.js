caee5192e03f6c2340cf6f8f6a208d7c126620d6
thinhnv <maxiqboy@gmail.com>
2020-08-29 08:47:45 +0700

    :pencil: [MINOR] Add Timeout env

--- src/common/config.js
+++ src/common/config.js
@@ -1,6 +1,6 @@
 export const TRN_API_URL =
   process.env.VUE_APP_API_URL || 'http://127.0.0.1:6969/api/v1/';
-export const TRN_TIME_OUT = 2000;
+export const TRN_TIME_OUT = process.env.VUE_APP_TIME_OUT || 2000;
 
 export const TRN_GLOBAL_TRANSITION = 'scroll-y-transition';
 
